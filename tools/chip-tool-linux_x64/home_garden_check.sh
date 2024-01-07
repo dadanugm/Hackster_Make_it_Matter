d296021c262537e3a1bafde665de515d29cbb197
dadanugm <dadanugm07@gmail.com>
2024-01-07 10:22:13 +0800

    initial commit

--- tools/chip-tool-linux_x64/home_garden_check.sh
+++ tools/chip-tool-linux_x64/home_garden_check.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
+
 echo soil sensor value
 > dump.txt
 ./chip-tool-debug levelcontrol read current-level 1 1 >> dump.txt
-grep -oP "(?<=Data = ).*(?=,)" dump.txt
+SensorVal="$(grep -oP "(?<=Data = ).*(?=,)" dump.txt)"
+MaxVal=100
+MinVal=40
+
+if [[($SensorVal -gt $MaxVal)]]
+then
+	echo "Please water your plant"
+elif [[($SensorVal -lt $MaxVal) && ($SensorVal -gt $MinVal)]]
+then
+	echo "Soil water is normal"
+elif [[($SensorVal -lt $MinVal)]]
+then
+	echo "too much water"
+fi
+
+echo "$SensorVal"
